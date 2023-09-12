78e2a4d8575a1f69a80d2480ca6088c2b5a67a8a
dani <daniyarabsatov@gmail.com>
2023-09-12 16:38:01 +0600

    wip

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+import asyncio
+import aioconsole
+
 from config import (
     SERVER_HOST,
     SERVER_PORT,
@@ -6,6 +9,9 @@ from asyncio import (
     StreamReader,
     StreamWriter,
 )
+import logger
+
+logger = logger.get_logger(__name__)
 
 
 class Client:
@@ -15,12 +21,40 @@ class Client:
         self.reader: StreamReader = None
         self.writer: StreamWriter = None
 
-    async def send(self, message=""):
-        pass
+    async def connect_to_server(self):
+        logger.info("Client connection is initialized at %s:%d", self.server_host, self.server_port)
+        try:
+            self.reader, self.writer = await asyncio.open_connection(
+                self.server_host, self.server_port,
+            )
+            await asyncio.gather(
+                self.send_messages(),
+                self.receive_messages(),
+            )
+        except Exception as ex:
+            logger.error("An error has occurred: %s", ex)
+        finally:
+            await self.close_connection()
 
-    async def connect(self):
-        pass
+    async def close_connection(self):
+        logger.info("Shutting down client connection at %s:%d", self.server_host, self.server_port,)
+        self.writer.close()
+        await self.writer.wait_closed()
 
-    async def receive(self):
-        pass
+    async def send_messages(self):
+        while True:
+            logger.info("Processing console input")
+            if message := await aioconsole.ainput("~~~ "):
+                self.writer.write(message.encode('utf-8'))
+                await self.writer.drain()
 
+    async def receive_messages(self):
+        while message := (await self.reader.read(1024)).decode():
+            logger.info("Message is received: %s", message)
+            await aioconsole.aprint(message)
+
+
+if __name__ == '__main__':
+    logger.info("Starting client connection")
+    client = Client()
+    asyncio.run(client.connect_to_server())
--- server.py
+++ server.py
@@ -1,9 +1,30 @@
-# import asyncio
+from config import (
+    SERVER_HOST,
+    SERVER_PORT,
+)
 
 
 class Server:
-    def __init__(self, host="127.0.0.1", port=8000):
-        pass
+    def __init__(self, host=SERVER_HOST, port=SERVER_PORT,):
+        self.host = host
+        self.port = port
 
-    def listen(self):
+    async def listen(self):
+        try:
+            server = await asyncio.start_server(
+                self.process_client, self.host, self.port,
+            )
+            logger.info("Server is initialized at %s", server.sockets[0].getsockname())
+            async with server:
+                await server.serve_forever()
+        except Exception as e:
+            logger.error("Error detected: %s", e)
+        except KeyboardInterrupt:
+            logger.info("Shutting down the server")
+
+
+    def process_client(self, reader: StreamReader, writer: StreamWriter):
+        # add client
+        # handle messages
+        # remove client
         pass
